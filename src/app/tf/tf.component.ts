3d210bd7567e6819db67ece414e982c1103fda1e
portmann <portmann.samu@gmail.com>
2017-09-09 14:50:44 +0200

    trallala

---
+++ src/app/tf/tf.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tf',
+  templateUrl: './tf.component.html',
+  styleUrls: ['./tf.component.css']
+})
+export class TfComponent implements OnInit {
+
+  answer1;
+  answer2;
+  answer3;
+  answer4;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
