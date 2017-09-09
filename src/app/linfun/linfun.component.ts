3f6972188d1c0cdc4f839ad2eee9d21e7f96f0c2
bersling <bersling@gmail.com>
2017-09-09 17:26:19 +0200

    working wip

---
+++ src/app/linfun/linfun.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-linfun',
+  templateUrl: './linfun.component.html',
+  styleUrls: ['./linfun.component.css']
+})
+export class LinfunComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
